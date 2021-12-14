2ea02982a177ccc5117822d8932061d538a1ace1
seo young choi <csy4723@gmail.com>
2021-12-15 01:56:38 +0900

    ??

--- MyProject/src/practice/Practice.java
+++ MyProject/src/practice/Practice.java
@@ -3,9 +3,22 @@ package practice;
 public class Practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i<9; i++) {
+			for(int j = 0; j < 0; j++) {
+				int point = 8;
+				
+				if(j >= point-i) {
+					System.out.println("*");
+				}else {
+					System.out.println(" ");
+				}
+				System.out.println();
+			}
+		}
 		
+
+			
 	}
 
+		
 }

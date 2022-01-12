41daa5077ea9ce6e138f7c9dd566b1ea649f7d0c
seo young choi <csy4723@gmail.com>
2022-01-12 21:48:59 +0900

    커밋

--- ReadingNote/src/com/me/view/openMenu.java
+++ ReadingNote/src/com/me/view/openMenu.java
@@ -15,6 +15,7 @@ public class openMenu {
 			System.out.println("2. 작성글 보기");
 			System.out.println("3. 작성글 검색하기");
 			System.out.println("4. 책 보기");
+			System.out.println("9. 프로그램 종료");
 			System.out.println("메뉴 입력 :");
 			int menu = sc.nextInt();
 			sc.nextLine();
@@ -28,6 +29,8 @@ public class openMenu {
 				break;
 			case 4:
 				break;
+			case 9: 
+				return;
 			default:
 				System.out.println("잘못 입력했습니다.");
 				break;

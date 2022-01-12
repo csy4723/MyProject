29d7063eaaf777ab07e4aa5a58ad2dc968df8ac6
seo young choi <csy4723@gmail.com>
2022-01-12 21:45:09 +0900

    메뉴

---
+++ ReadingNote/src/com/me/run/Run.java
@@ -0,0 +1,13 @@
+package com.me.run;
+
+import com.me.view.openMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+	
+		openMenu o = new openMenu();
+		o.mainMenu();
+	}
+
+}
---
+++ ReadingNote/src/com/me/view/openMenu.java
@@ -0,0 +1,39 @@
+package com.me.view;
+
+import java.util.Scanner;
+
+public class openMenu {
+	Scanner sc = new Scanner(System.in);
+
+	public void mainMenu() {
+
+		while (true) {
+
+			System.out.println("===메뉴===");
+			System.out.println();
+			System.out.println("1. 독서 노트 작성하기");
+			System.out.println("2. 작성글 보기");
+			System.out.println("3. 작성글 검색하기");
+			System.out.println("4. 책 보기");
+			System.out.println("메뉴 입력 :");
+			int menu = sc.nextInt();
+			sc.nextLine();
+
+			switch (menu) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default:
+				System.out.println("잘못 입력했습니다.");
+				break;
+			}
+
+		}
+	}
+
+}

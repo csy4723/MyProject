1c4dfb9ff849b5bf8c77f00cc1992b70220c87f8
seo young choi <csy4723@gmail.com>
2022-01-12 21:39:40 +0900

    2번째

--- ReadingNote/src/com/me/model/vo/Note.java
+++ ReadingNote/src/com/me/model/vo/Note.java
@@ -1,6 +1,57 @@
 package com.me.model.vo;
 
+import java.util.Calendar;
+
 public class Note {
 	private String title;
+	private String content; 
+	private Calendar date;
+	private String userId;
+	
+	public Note() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Note(String title, String content, Calendar date, String userId) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.date = date;
+		this.userId = userId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
 
 }

8c5b8a977c696a194eb7cb51a303d848cc06580f
seo young choi <csy4723@gmail.com>
2022-01-16 22:42:47 +0900

    dd

--- ReadingNote/src/com/me/controller/Notecontroller.java
+++ ReadingNote/src/com/me/controller/Notecontroller.java
@@ -5,17 +5,28 @@ import java.util.Scanner;
 public class Notecontroller {
 	Scanner sc = new Scanner(System.in);
 	StringBuffer sb = new StringBuffer();
+
 	public Notecontroller() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public void writeNote() {
 		System.out.println("제목 : ");
 		String title = sc.nextLine();
-		System.out.println("내용 : ");
-		sb.append(sc.nextLine());
+		System.out.println("내용 : \n (종료 : EXIT)");
 		
+		String str = "";
+		while (true) {
+
+			str = sc.nextLine();
+			if(!str.equalsIgnoreCase("exit")) {
+				sb.append(str);
+			}else {
+				break;
+			}
+			
+			
+		}
 	}
-	
-	
 
 }

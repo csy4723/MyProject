9652599c96b92745cdaaa8c0d0bd8839b01718be
seo young choi <csy4723@gmail.com>
2022-01-20 21:24:19 +0900

    북 파일 저장 시스템 완료 아직 테스트는 안함

--- ReadingNote/src/com/me/controller/Bookcontroller.java
+++ ReadingNote/src/com/me/controller/Bookcontroller.java
@@ -1,5 +1,76 @@
 package com.me.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.me.model.vo.Book;
+
 public class Bookcontroller {
+	private int bCnt = 0;
+	
+	ArrayList<Book> book = new ArrayList<Book>();
+	Scanner sc = new Scanner(System.in);
+	
+	
+	public void saveBook() {
+		
+		File ex = new File("Book.ser");
+		
+		if(ex.exists()) {
+			ex.delete();
+		}
+		
+		
+		File f = new File("Book.ser");
+		
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f.getName()))){
+			
+			oos.writeObject(book);
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	public void readBook() {
+		
+		
+		File f = new File("Book.ser");
+		
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f.getName()))){
+			
+			
+			book = (ArrayList<Book>)ois.readObject();
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("읽은 책 파일이 존재 하지 않습니다. ");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
 
 }
--- ReadingNote/src/com/me/controller/Notecontroller.java
+++ ReadingNote/src/com/me/controller/Notecontroller.java
@@ -17,12 +17,10 @@ import com.me.view.openMenu;
 public class Notecontroller {
 	Scanner sc = new Scanner(System.in);
 	StringBuffer sb = new StringBuffer();
-	static ArrayList<Note> note = new ArrayList<>();
+	ArrayList<Note> note = new ArrayList<>();
 	Note n = new Note();
 
-	public Notecontroller() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 	public void writeNote() {
 		System.out.println("제목 : ");
